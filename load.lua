e4b40344338ed9d4bad38373c740035abbecfaf7
NYXOXOV3 <fortressjr25@gmail.com>
2025-11-23 00:40:31 +0700

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,8 @@
+getgenv().Config = {
+    Headless = true,
+
+    FakeDisplayName = "Solace ",
+    FakeName = "Solace",
+    FakeId = 13886182, -- input id
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NYXOXOV3/hide/main/main.lua"))()
